788e37fae868441984a60def07378e8fcd4836bc
Bob Coleman (skilldown) <bobcolem@gmail.com>
2016-10-10 14:04:07 -0400

    Reduce memory consumption when compiling syntax-case.
    Added gcc options to reduce the memory required when compiled
    syntax-case as part of sphere install at the expense of runtime speed.
    
    Ignore the temp files generated during the build.

--- spheres/gambit/compile-scsc.sh
+++ spheres/gambit/compile-scsc.sh
@@ -2,5 +2,5 @@
 cd scsc
 if test -e syntax-case.o1; then rm syntax-case.o1; fi
 gsi -f boot.scm
-gsc -f -e "(parameterize ((current-readtable (readtable-sharing-allowed?-set (current-readtable) 'serialize))) (compile-file \"syntax-case\"))"
+gsc -f -e "(parameterize ((current-readtable (readtable-sharing-allowed?-set (current-readtable) 'serialize))) (compile-file \"syntax-case\" cc-options: \"-ftrack-macro-expansion=0 -save-temps -D___SINGLE_HOST -D___OPTIMIZE_SPACE -O0\"))"
 cp syntax-case.o1 ..
